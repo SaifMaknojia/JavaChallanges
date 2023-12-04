2400e61cecb41d91febe72d8d98389b4f915a4c8
Saif Maknojia <saifmaknojia@Saifs-MacBook-Pro.local>
2023-12-03 20:12:36 -0500

    completed 3rd challenge

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,17 @@
 package org.example;
 
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         System.out.println("console log");
         findEvenNumber();
+
+        //feel to change any Parameter's you want.
         sumOfNumber(12345);
+        multiplicationTable(5);
     }
 
     //Write a program to print all even numbers between 1 - 100.
@@ -15,6 +20,13 @@ public class Main {
             if(i % 2 == 0){
                 System.out.println(i);
             }
+        }
+    }
+
+    //Write a program to print multiplication table of any number.
+    public static void multiplicationTable(int integer){
+        for(int i = 0; i <= 12; i++){
+            System.out.println(i * integer);
         }
     }
 
@@ -28,8 +40,5 @@ public class Main {
         }
         System.out.println(total);
     }
-
-
-
 
 }
